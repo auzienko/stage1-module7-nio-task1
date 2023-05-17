8932f5ece55518777efce21732cb759aa059ff81
Aleksandr Uzienko <a.uzienko@gmail.com>
2023-05-17 17:03:30 +0300

    solution

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Path path = Path.of(file.toURI());
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(Files.newInputStream(path)))) {
+            Map<String, String> keyValue = new HashMap<>();
+            while (br.ready()) {
+                String[] split = br.readLine().split(":");
+                keyValue.put(split[0], split[1].strip());
+            }
+
+            return new Profile(
+                    keyValue.get("Name"),
+                    Integer.parseInt(keyValue.get("Age")),
+                    keyValue.get("Email"),
+                    Long.parseLong(keyValue.get("Phone")));
+
+        } catch (IOException e) {
+            throw new MyOwnException(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/epam/mjc/nio/MyOwnException.java
@@ -0,0 +1,7 @@
+package com.epam.mjc.nio;
+
+public class MyOwnException extends RuntimeException{
+    public MyOwnException(String message) {
+        super(message);
+    }
+}
